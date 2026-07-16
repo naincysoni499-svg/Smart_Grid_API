4e0c55b14cb48abe0fd33c1bd1701a5d3dbc6653
naincysoni499-svg <naincysoni499@gmail.com>
2026-07-16 21:58:05 +0530

    Week 1: Implement telemetry API endpoint

--- app.py
+++ app.py
@@ -1,10 +1,31 @@
-from flask import Flask
+from fastapi import FastAPI
+from database import engine, SessionLocal
+from models import Base, SmartMeterData
+from schemas import MeterData
 
-app = Flask(__name__)
+Base.metadata.create_all(bind=engine)
 
-@app.route("/")
+app = FastAPI()
+
+@app.get("/")
 def home():
-    return "Smart Grid API is Running!"
+    return {"message": "Smart Grid API is Running"}
 
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.post("/telemetry")
+def add_data(data: MeterData):
+    db = SessionLocal()
+
+    meter = SmartMeterData(
+        meter_id=data.meter_id,
+        timestamp=data.timestamp,
+        power_usage=data.power_usage,
+        voltage=data.voltage,
+        current=data.current,
+        frequency=data.frequency
+    )
+
+    db.add(meter)
+    db.commit()
+    db.close()
+
+    return {"message": "Telemetry data stored successfully"}
